1a4873648371a6b019f18e962329880620e71e5f
Nathaniel Flores <71289838+nsflores1@users.noreply.github.com>
2026-09-15 17:47:29 -0400

    Measure success and failure with compression

--- cadabby-core/src/lib.rs
+++ cadabby-core/src/lib.rs
@@ -1,33 +1,122 @@
 use fastcdc::v2020::FastCDC;
 use blake3::Hash;
-use std::io::Result;
-use zstd::bulk::{compress, decompress};
+use zstd::bulk::{Compressor, Decompressor};
+use rayon::prelude::*;
+use std::cell::RefCell;
+use std::{fmt, hash};
 
-/// Splits with FastCDC & hashes with BLAKE3.
-pub fn chunkfile(file: &[u8]) -> Vec<(usize, usize, Hash)> {
-    FastCDC::new(file, 262144, 1048576, 4194304)
+const CHUNK_MAXSIZE: usize = 4194304;
+const CHUNK_AVGSIZE: usize = 1048576;
+const CHUNK_MINSIZE: usize = 262144;
+
+#[allow(dead_code)]
+/// TODO: implement Eq, remove PartialEq 
+/// Also, implement Ord
+#[derive(Debug, Clone, PartialEq)]
+pub struct Chunk {
+    pub compressed: bool,
+    data: Vec<u8>,
+    hash: Hash,
+    offset: usize,
+    length: usize,
+    pub savings: usize,
+}
+
+/// TODO: this could be better
+impl hash::Hash for Chunk {
+    fn hash<H: hash::Hasher>(&self, state: &mut H) {
+        self.data.hash(state);
+    }
+}
+
+impl fmt::Display for Chunk {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Chunk(con: {}, off: {}kb, len: {}kb)",
+            self.compressed,
+            self.offset as f32 / 1000.0,
+            self.offset as f32 / 1000.0,
+        )
+    }
+}
+
+/// TODO: this could be better
+impl fmt::Pointer for Chunk {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let ptr = self as *const Self;
+        fmt::Pointer::fmt(&ptr, f)
+    }
+}
+
+/// Splits with FastCDC, then hashes with BLAKE3.
+pub fn chunkfile(file: &[u8]) -> Vec<Chunk> {
+    FastCDC::new(file, CHUNK_MINSIZE, CHUNK_AVGSIZE, CHUNK_MAXSIZE)
         .map(|chunk| {
-            let part = &file[chunk.offset..chunk.offset + chunk.length];
-            let hash = blake3::hash(part);
-            (chunk.offset, chunk.length, hash)
+            let data = file[chunk.offset..chunk.offset + chunk.length].to_vec();
+            let hash = blake3::hash(&data);
+            Chunk {
+                compressed: false,
+                data,
+                hash,
+                offset: chunk.offset,
+                length: chunk.length,
+                savings: 0,
+            }
         })
         .collect()
 }
 
-#[allow(dead_code)]
-pub struct Chunk {
-    compressed: bool,
-    data: Vec<u8>,
-    hash: Option<Hash>,
+/// Attempts to compress a Vec<Chunk> to ZSTD in parallel.
+pub fn chunkcompress(mut chunks: Vec<Chunk>) -> Vec<Chunk> {
+    thread_local! {
+        // lifetime for an optional dict; we aren't using it
+        static COMP: RefCell<Compressor<'static>> = RefCell::new(
+            Compressor::new(0).expect("failed to load zstd")
+        );
+    }
+
+    chunks.par_iter_mut().for_each(|chunk| {
+        COMP.with(|c| {
+            if !chunk.compressed {
+                let mut buf = Vec::with_capacity(CHUNK_MAXSIZE);
+                let Ok(size) = c
+                    .borrow_mut()
+                    .compress_to_buffer(&chunk.data, &mut buf) else { return };
+            
+                if size < chunk.data.len() {                
+                    chunk.compressed = true;
+                    chunk.savings = chunk.data.len() - size;
+                    chunk.data = buf;
+                }
+            }
+        });
+    });
+
+    chunks
 }
 
-/// Attempts to compress a chunk to ZSTD.
-pub fn chunkshrink(chunk: &[u8]) -> Result<Vec<u8>> {
-    compress(chunk, 0)
-}
+/// Attempts to decompress a Vec<Chunk> from ZSTD in parallel.
+pub fn chunkexpand(mut chunks: Vec<Chunk>) -> Vec<Chunk> {
+    thread_local! {
+        // lifetime for an optional dict; we aren't using it
+        static DECOMP: RefCell<Decompressor<'static>> = RefCell::new(
+            Decompressor::new().expect("failed to load zstd")
+        );
+    }
 
-/// Attempts to decompress a chunk from ZSTD.
-pub fn chunkexpand(chunk: &[u8]) -> Result<Vec<u8>> {
-    decompress(chunk, 0)
+    chunks.par_iter_mut().for_each(|chunk| {
+        DECOMP.with(|d| {
+            if chunk.compressed {
+                let mut buf = Vec::with_capacity(CHUNK_MAXSIZE);
+                let Ok(_) = d
+                    .borrow_mut()
+                    .decompress_to_buffer(&chunk.data, &mut buf) else { return };
+                
+                chunk.compressed = false;
+                chunk.data = buf;
+            }
+        });
+    });
+
+    chunks
 }
 
--- cadabby/src/main.rs
+++ cadabby/src/main.rs
@@ -1,4 +1,4 @@
-use cadabby_core::chunkfile;
+use cadabby_core::{chunkfile, chunkcompress};
 use std::env::args;
 use std::path::PathBuf;
 use std::fs;
@@ -11,6 +11,22 @@ fn main() {
 
     let data = fs::read(path).expect("file should exist");
     let splits = chunkfile(&data);
+    
     println!("total chunk count: {}", &splits.len());
+    
+    let compress = chunkcompress(splits);
+    let compnum: Vec<_> = compress
+        .into_iter()
+        .filter(|x| x.compressed)
+        .collect();
+        
+    println!("total compressed chunks: {}", &compnum.len());
+
+    let mut compsave = 0;
+    for chunk in compnum {
+        compsave += chunk.savings; 
+    }
+
+    println!("compression saved {}MB", compsave as f32 / 1_000_000.0);
     println!("done");
 }
